44ddf308a6cd484c142537efb83a83ebaa167bb5
kevinguyen76 <62038441+kevinguyen76@users.noreply.github.com>
2021-09-10 11:22:15 +1000

    fix: ts build errors

--- src/services/reviews/delete-review.ts
+++
@@ -1,3 +0,0 @@
-import { Review } from "@prisma/client";
-
-import { getDB } from "@app/lib/db";
--- src/services/reviews/index.ts
+++ src/services/reviews/index.ts
@@ -1,5 +1,2 @@
 export * from "./create-review";
-export * from "./delete-review";
-export * from "./get-movie-review";
 export * from "./get-user-reviews";
-export * from "./update-review";
