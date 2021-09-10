c39d7fd5af19b03db4156027c1e4cc3def4641b6
kevinguyen76 <62038441+kevinguyen76@users.noreply.github.com>
2021-09-10 11:28:57 +1000

    fix: movieReview esLint fix

--- src/pages/users.tsx
+++ src/pages/users.tsx
@@ -18,7 +18,7 @@ const UsersPage: NextPage = () => (
       <p> 3 👎🏼</p>
     </HStack>
     <UserProfile name="Harry Potter 🧙‍♂️"/>
-    <p>Some of the scenes are primarily comic, letting the stars banter and interrupt each other and talk over each other and make pleas and snarky asides to the camera. The more dramatic scenes are photographed "straight," and there are times when one actor or the other effectively seizes the spotlight, as they might in a stage production, and delivers a long monologue. </p>
+    <p>Some of the scenes are primarily comic, letting the stars banter and interrupt each other and talk over each other and make pleas and snarky asides to the camera. The more dramatic scenes are photographed straight, and there are times when one actor or the other effectively seizes the spotlight, as they might in a stage production, and delivers a long monologue. </p>
 
     <Stack paddingBottom="1rem">
       <HStack>
@@ -29,7 +29,7 @@ const UsersPage: NextPage = () => (
         <p> 3 👎🏼</p>
       </HStack>
       <UserProfile name="Harry Potter 🧙‍♂️"/>
-      <p>Some of the scenes are primarily comic, letting the stars banter and interrupt each other and talk over each other and make pleas and snarky asides to the camera. The more dramatic scenes are photographed "straight," and there are times when one actor or the other effectively seizes the spotlight, as they might in a stage production, and delivers a long monologue. </p>
+      <p>Some of the scenes are primarily comic, letting the stars banter and interrupt each other and talk over each other and make pleas and snarky asides to the camera. The more dramatic scenes are photographed straight, and there are times when one actor or the other effectively seizes the spotlight, as they might in a stage production, and delivers a long monologue. </p>
     </Stack>
       
     <HStack>
@@ -40,7 +40,7 @@ const UsersPage: NextPage = () => (
       <p> 3 👎🏼</p>
     </HStack>
     <UserProfile name="Harry Potter 🧙‍♂️"/>
-    <p>Most of the time movies are anticipated like this they end up falling short, way short. Joker is the first time I was more than happy with the hype. Please ignore the complaints of "pernicious violence" as they are embarrassing to say the least. We haven't seen a comic movie this real before. If we ever "deserved" a better class of criminal - Phillips and Phoenix have delivered. This is dark, Joker IS dark and you will fall in love with the villain as you should. The bad guys are always more romantic anyway.</p>
+    <p>Most of the time movies are anticipated like this they end up falling short, way short. Joker is the first time I was more than happy with the hype. Please ignore the complaints of pernicious violence as they are embarrassing to say the least. We havent seen a comic movie this real before. If we ever deserved a better class of criminal - Phillips and Phoenix have delivered. This is dark, Joker IS dark and you will fall in love with the villain as you should. The bad guys are always more romantic anyway.</p>
 git   </div>
 );
 
