c9ec4bc3d600fab216240f7cf41eb8e71f90244b
Dennis Zub <3003dennis@gmail.com>
2021-10-04 23:01:22 +1100

    feat: added unit tests

--- src/services/__tests__/movies/get-sample-movie.spec.ts
+++ src/services/__tests__/movies/get-sample-movie.spec.ts
@@ -3,13 +3,13 @@ import fetch from 'node-fetch';
 import { mocked } from 'ts-jest/utils';
 
 // Import the service to be tested
-import { getPopularMovies } from '@app/services/movie/get-popular-movies';
+import { getMovie } from '@app/services/movie/get-movie';
 
 // Set node-fetch to be used globally during execution of the test
 global.fetch = require("node-fetch");
 
 // Load the sample data to be passed into the function
-const popularMoviesSample = require('./popularMoviesData.json');
+const movieSample = require('./sampeMovieData.json');
 
 // Tell Jest to mock node-fetch
 jest.mock('node-fetch', () => {
@@ -24,20 +24,26 @@ beforeEach(() => {
 // The test
 // Jest is configured to mock the fetch function in the servic
 // When the fetch function is called, the sample data is returned
-describe('getPopularMovies test', () => {
-  test('getPopularMovies should retrieve 20 popular movies', async () => {
+describe('getMovie test', () => {
+  test('getMovie should retrieve the Dont Breathe movie', async () => {
     mocked(fetch).mockImplementation((): Promise<any> => {
       return Promise.resolve({
         json() {
-          return Promise.resolve(popularMoviesSample);
+          return Promise.resolve(movieSample);
         }
       });
     });
 
-    // Call the getPopularMovies function and await for it to return processed results
-    const popularMovies = await getPopularMovies();
+    // Call the get movie service and await for it to return a processed response
+    const movie = await getMovie('300669');
+
+    // Check that the processed movie title matches the expected value
+    expect(movie.title).toEqual("Don't Breathe");
     
-    // Check that the length of the returned array is equal to 20 
-    expect(popularMovies).toHaveLength(20);
+    // Check that the movie ID matches the exepcted value
+    expect(movie.id).toEqual(300669);
+    
+    // Check that the release status matches the expected value
+    expect(movie.status).toEqual("Released");
   })
 })
