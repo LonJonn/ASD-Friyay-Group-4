8b71dd43071a8615c3ff0979ee06e934c136f575
Jyuston <justinyuen2000@hotmail.com>
2021-10-07 22:39:39 +1100

    fix: unused import error

--- src/pages/api/comments/index.ts
+++ src/pages/api/comments/index.ts
@@ -1,7 +1,6 @@
 import { NextApiHandler } from "next";
 import { getSession } from "next-auth/client";
 import { getReportedMovieComments, updateReportedMovieCommentToTrue } from "@app/services/comment/";
-import Id from "@app/pages/groups/[id]";
 
 const handler: NextApiHandler = async (req, res) => {
   const session = await getSession({ req });
