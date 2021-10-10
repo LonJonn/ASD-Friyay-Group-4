e976c37fe6ed888900094c8cf25790809e207db2
Dennis Zub <3003dennis@gmail.com>
2021-10-10 17:28:13 +1100

    feat: added routing to actor

--- src/components/movie/ActorPreviewCard.tsx
+++ src/components/movie/ActorPreviewCard.tsx
@@ -1,4 +1,6 @@
-import { Box, Image, Text } from "@chakra-ui/react";
+import { LinkBox, Box, Image, Text, LinkOverlay } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useQueryClient } from "react-query";
 
 interface IActorPreviewCard {
   key: number;
@@ -8,41 +10,51 @@ interface IActorPreviewCard {
   profile_path: string;
 }
 
-const PopularMovieCard: React.FC<IActorPreviewCard> = ({ name, character, profile_path  }) => {
-  return (
-    <Box borderWidth="1px" borderRadius="lg" overflow="hidden" boxShadow="2xl" p={4} display={{ md: "flex" }}>
-        {/* The poster photo for each 'card' */}
-        <Box flexShrink={0}>
-            <Image
-                borderRadius="lg"
-                width="75px"
-                src={profile_path}
-                alt="Movie Poster"
-            />
-        </Box>
+const PopularMovieCard: React.FC<IActorPreviewCard> = ({ id, name, character, profile_path  }) => {
+    const router = useRouter();
+    const queryClient = useQueryClient();
 
-        <Box mt={{ base: 4, md: 0 }} ml={{ md: 6 }}>      
-            {/* Rendering of the nested box element, with the actor's name */}
-            <Box
-                fontWeight="bold"
-                letterSpacing="wide"
-                fontSize="s"
-                textTransform="uppercase"
-                textAlign="left"
-            >
-                <Text>
-                    {name}
-                </Text>
+    const handleClick = () => {
+        queryClient.removeQueries("getMovie");
+        router.push(`/actors/${id}`);
+    };
+
+    return (
+        <LinkBox borderWidth="1px" borderRadius="lg" overflow="hidden" boxShadow="2xl" p={4} display={{ md: "flex" }} onClick={handleClick}>
+            {/* The poster photo for each 'card' */}
+            <Box flexShrink={0}>
+                <Image
+                    borderRadius="lg"
+                    width="75px"
+                    src={profile_path}
+                    alt="Movie Poster"
+                />
+            </Box>
+
+            <Box mt={{ base: 4, md: 0 }} ml={{ md: 6 }}>      
+                {/* Rendering of the nested box element, with the actor's name */}
+                <Box
+                    fontWeight="bold"
+                    letterSpacing="wide"
+                    fontSize="s"
+                    textTransform="uppercase"
+                    textAlign="left"
+                >
+                    <LinkOverlay href="#">
+                        <Text>
+                            {name}
+                        </Text>
+                    </LinkOverlay>
+                    
+                </Box>
                 
+                {/* Rendering of the character text*/}
+                <Box color="gray.500" fontWeight="semibold" letterSpacing="wide" fontSize="xs" textTransform="uppercase">
+                    {character}
+                </Box>
             </Box>
-            
-            {/* Rendering of the character text*/}
-            <Box color="gray.500" fontWeight="semibold" letterSpacing="wide" fontSize="xs" textTransform="uppercase">
-                {character}
-            </Box>
-        </Box>
-    </Box>
-  );
+        </LinkBox>
+    );
 };
 
 export default PopularMovieCard;
